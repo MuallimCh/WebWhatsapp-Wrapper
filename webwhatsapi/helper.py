3fa7a1c8589fc78fa42d7681c690419c4398616d
LiemCH <muallimch@yahoo.com>
2018-11-15 07:53:57 +0700

    Update helper.py
    Convert any text value to boolean

--- webwhatsapi/helper.py
+++ webwhatsapi/helper.py
@@ -2,7 +2,7 @@ import six
 
 
 def safe_str(text):
-    if not text:
+    if not bool(text):
         return "(empty)"
     assert isinstance(text, six.string_types), "obj is not a string: %r" % text
-    return str(text.encode('utf-8').decode('ascii', 'ignore')) if text else "(empty)"
+    return str(text.encode('utf-8').decode('ascii', 'ignore')) if bool(text) else "(empty)"
